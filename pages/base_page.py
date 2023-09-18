148bd703b487544ca882967e3215a0694caa22c9
bordovaya <bordovaya@mail.ru>
2023-09-18 12:09:31 +0300

    1

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,20 +1,17 @@
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-import time
 class BasePage:
 #
-    def __init__(self, driver):
+    def __init__(self, driver, base_url):
         self.driver = driver
-        self.base_url = 'https://demoqa.com/'
+        self.base_url = base_url #'https://demoqa.com/'
 
     def visit(self):
         return self.driver.get(self.base_url)
 
-    def find_element(self, locator):
-        time.sleep(3)
-        return self.driver.find_element(By.CSS_SELECTOR, locator)
     def get_url(self):
         return self.driver.current_url
+
+    def equal_url(self):
+        return self.get_url() == self.base_url
 
 
 
--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,22 +1,11 @@
 from conftest import browser
 from pages.demoqa import DemoQa
-#from selenium.webdriver.common.by import By
-#from selenium.common.exceptions import NoSuchElementException
-#
+
 def test_icon_exist(browser):
-    demo_page = DemoQa(browser)#создали страницу
-    demo_page.visit()#зашли на страницу
-    demo_page.click_on_the_icon()#клик по кнопке
-    assert demo_page.exist_icon()#проверили наличие элемента
+    demo_page = DemoQa(browser)  # создали страницу
+    demo_page.visit()  # зашли на страницу
+    demo_page.icon.click()  # клик по кнопке
+
+    assert demo_page.icon.exist()  # проверили наличие элемента
     assert demo_page.equal_url()
 
-
-
-
-    # browser.get('https://demoqa.com/')
-    #
-    # try:
-    #     browser.find_element(By.CSS_SELECTOR, '#app > header > a')
-    # except NoSuchElementException:
-    #     assert False
-    # assert True
