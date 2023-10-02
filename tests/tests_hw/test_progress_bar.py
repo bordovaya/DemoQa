73b6af3a3e8c93cbe8e00086c5762fc0b1c5419b
bordovaya <bordovaya@mail.ru>
2023-10-02 17:19:12 +0300

    1

--- tests/tests_hw/test_progress_bar.py
+++ tests/tests_hw/test_progress_bar.py
@@ -10,7 +10,7 @@ def test_progress(browser):
 
     progress_page.btn_start.click_force()
     time.sleep(3.1)
-    while progress_page.progress_bar.get_dom_attribute('valuenow')=='51':
+    if progress_page.progress_bar.get_dom_attribute('valuenow')=='51':
         return progress_page.btn_start.click_force()
     time.sleep(2)
 
